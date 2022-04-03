54065fc493e448250b87ac269a56d0e83a4974fa
ankotyandayo <shuto.ando1@gmail.com>
2022-04-03 23:48:08 +0900

    2022/4/3

--- app/Models/Key.php
+++ app/Models/Key.php
@@ -27,6 +27,29 @@ class Key extends Model
 
     public function admin()
     {
-        return $this->belongsTo(Admin::class);
+        // return $this->belongsTo(Admin::class);
+    }
+
+    public function getKey()
+    {
+        $query_tag = \Request::query('tag');
+
+        if (!empty($query_tag)) {
+            $keys = Key::select('keys.*', 'detailtags.id AS detailtag_id', 'detailtags.name AS detailtag_name',)
+                ->leftJoin('key_tags', 'key_tags.key_id', '=', 'keys.id')
+                // ->leftJoin('tags', 'key_tags.tag_id', '=', 'tags.id')
+                ->leftJoin('tag_detailtags', 'tag_detailtags.key_id', '=', 'keys.id')
+                ->leftJoin('detailtags', 'tag_detailtags.detailtag_id', '=', 'detailtags.id')
+                ->where('key_tags.tag_id', '=', $query_tag)
+                ->get();
+            // dd($keys);
+        } else {
+
+            $keys = Key::select('id', 'key_1', 'key_2', 'key_3', 'key_4', 'note', 'content')
+                ->whereNull('deleted_at')
+                ->orderBy('updated_at', 'DESC')
+                ->get();
+        }
+        return $keys;
     }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,26 +28,10 @@ class AppServiceProvider extends ServiceProvider
     {
         view()->composer('*', function ($view) {
             // \でインポートしなくても使える
-            $query_tag = \Request::query('tag');
-
-            if (!empty($query_tag)) {
-                $keys = Key::select('keys.*', 'detailtags.*',)
-                    ->leftJoin('key_tags', 'key_tags.key_id', '=', 'keys.id')
-                    // ->leftJoin('tags', 'key_tags.tag_id', '=', 'tags.id')
-                    ->leftJoin('tag_detailtags', 'tag_detailtags.key_id', '=', 'keys.id')
-                    ->leftJoin('detailtags', 'tag_detailtags.detailtag_id', '=', 'detailtags.id')
-                    ->where('key_tags.tag_id', '=', $query_tag)
-                    ->get();
-                // dd($keys);
-            } else {
-                $keys = Key::select('id', 'key_1', 'key_2', 'key_3', 'key_4', 'note', 'content')
-                    ->whereNull('deleted_at')
-                    ->orderBy('updated_at', 'DESC')
-                    ->get();
-            }
-
+            $key_model = new Key(); //インスタンス化
+            $keys = $key_model->getKey();
             $tags = Tag::where('admin_id', '=', \Auth::id())->get();
-
+            // dd($keys);
             $view->with('tags', $tags)->with('keys', $keys);
         });
     }
